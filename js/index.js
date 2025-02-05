620630631b5d5d8a81f1a4e55394180677dada8d
Anna <acesbrama@gmail.com>
2025-02-04 22:20:13 -0300

    refactor: section news__section para o index.js

--- js/index.js
+++ js/index.js
@@ -18,24 +18,81 @@ images.forEach(image => {
 })
 
 //colocando mensagem de boas vindas no nav através do button 
-const btn = document.querySelector("button");btn.addEventListener("click", welcome)
+const btn = document.querySelector("button").onclick = () => welcome()
 const userName = document.querySelector(".userName")
 
 userName.textContent = ""
 
-function welcome() {
-    let name = prompt("What's your name?")
-    if (!name.trim()) { 
-        alert("Please enter a valid name.")
-        return
-    }
+//obtendo o nome do usuário
+const getUserName = () => prompt("What's your name?")
+//salvando o nome do usuário no localStorage do navegador
+const saveName = (name) => localStorage.setItem('name', name)
+const createWelcomeMessage = (name) => `Welcome ${name}`
+//atualizando o nome do usuário já inserido anteriormente
+const updateUserName = (message) => (userName.textContent = message)
+
+const welcome = () => {
+    const name = getUserName()
     if (name) {
-        localStorage.setItem("name", name)
-        userName.textContent = `Hello, ${name}.`
+        saveName(name)
+        updateUserName(createWelcomeMessage(name))
     }
 }
 
-const storedName = localStorage.getItem("name")
-if (storedName) {
-    userName.textContent = `Hello, ${storedName}.`
+const newsSection = () => {
+    let section = document.querySelector('.news__section')
+    let container = section.querySelector('.container')
+    container.innerHTML = `
+                <div class="news__title">
+                <h2>Last updated albums</h2>
+            </div>
+            <div class="news__menu">
+                <div class="news__photo">
+                    <img src="assets/sections/merino-celebrating-brentford.jpg"
+                        alt="Mikel Merino celebrating against Brentford - Premier League" loading="lazy">
+                </div>
+                <div class="news__albums">
+                    <div class="album__name">
+                        <a href="">
+                            <span>Premier League</span>
+                            <p>01/01 at BRENTFORD</p>
+                        </a>
+                    </div>
+                    <div class="album__name">
+                        <a href="">
+                            <span>Premier League</span>
+                            <p>12/27 IPSWITCH TOWN</p>
+                        </a>
+                    </div>
+                    <div class="album__name">
+                        <a href="">
+                            <span>Premier League</span>
+                            <p>12/21 at CRYSTAL PALACE</p>
+                        </a>
+                    </div>
+                    <div class="album__name">
+                        <a href="">
+                            <span>Carabao Cup</span>
+                            <p>12/18 CRYSTAL PALACE</p>
+                        </a>
+                    </div>
+                    <div class="album__name">
+                        <a href="">
+                            <span>Premier League</span>
+                            <p>12/14 EVERTON</p>
+                        </a>
+                    </div>
+                    <div class="album__name">
+                        <a href="">
+                            <span>Champions League</span>
+                            <p>12/11 MONACO</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
 }
+
+//ativação da função para carregamento da section news__section
+newsSection()
